0625b813a949c57a1f26490893e7549a2fb0c6c3
Richard J Harding <richard.jharding@hotmail.co.uk>
2022-01-29 21:12:28 +0000

    Initial commit

---
+++ assets/js/quiz.js
@@ -0,0 +1,9 @@
+/* File used by quiz.html in order to display a quiz which is made up of 10
+questions for the user to answer. The quiz will be run with a timer and a way
+of displaying progress with a bar showing what % of questions they have answered
+so far and can guage how many are left. There will also be a resulting score and 
+summary screen. */
+
+// List of variables used for the Quiz
+
+
