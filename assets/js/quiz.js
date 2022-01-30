0715dc3e909e137972a2fcfbfc461aa1a918de2e
Richard J Harding <richard.jharding@hotmail.co.uk>
2022-01-30 23:05:20 +0000

    Feat: Add question one and correct answer

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -12,3 +12,16 @@ let quizScore = document.querySelector('#score');
 let quizProgressMax = document.querySelector('#quiz-progress-max');
 
 let question = document.querySelector('#question');
+
+// Array for questions to appear in quiz
+let questions = [
+    {
+        question: "Which of these mountains are NOT in Colombia?",
+        option1: "Pico Simón Bolívar",
+        option2: "Ritacuba Blanco",
+        option3: "Ben Nevis",
+        option4: "Pico Cristóbal Colón",
+
+        answer: 3
+    }
+]
