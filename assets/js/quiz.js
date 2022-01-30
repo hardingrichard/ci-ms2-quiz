062fa8f899a55818a7f3546601abe657d65a70f2
Richard J Harding <richard.jharding@hotmail.co.uk>
2022-01-30 22:48:52 +0000

    Refactor: Declare variables for quiz header and questions

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,9 +1,14 @@
 /* File used by quiz.html in order to display a quiz which is made up of 10
-questions for the user to answer. The quiz will be run with a timer and a way
-of displaying progress with a bar showing what % of questions they have answered
-so far and can guage how many are left. There will also be a resulting score and 
-summary screen. */
+questions for the user to answer. The quiz will be run with a progress bar and 
+question count showing the progress of the quiz so that the user can guage how 
+many questions are left. The score will increment at the top to give feedback
+to the user on how well they are doing.
+*/
 
 // List of variables used for the Quiz
 
+let quizProgressTitle = document.querySelector('#quiz-prog-title');
+let quizScore = document.querySelector('#score');
+let quizProgressMax = document.querySelector('#quiz-progress-max');
 
+let question = document.querySelector('#question');
