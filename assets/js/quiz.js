c362cf9b621d5da5954e3630ca75798c3da5313c
Richard J Harding <richard.jharding@hotmail.co.uk>
2022-01-31 23:11:33 +0000

    Feat: Add modals, function and style

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -14,7 +14,7 @@ let quizScore = document.getElementById('score');
 let points_score = 10;
 let score = 0;
 
-// progress bar variables - Resource used for help: https://javascript.plainenglish.io/building-a-progress-bar-in-css-js-html-from-scratch-6449da06042
+// progress bar variables - Resource used for help and adapted: https://javascript.plainenglish.io/building-a-progress-bar-in-css-js-html-from-scratch-6449da06042
 let quizProgressTitle = document.getElementById('quiz-progress-title');
 let quizProgressMax = document.getElementById('quiz-progress-max');
 let questionCounter = 0;
@@ -30,52 +30,78 @@ let options = Array.from(document.querySelectorAll('.answer-option'));
 let correctAnswer = true
 
 // Clicking on the start button will begin quiz
-// let startButton = document.getElementById('start-btn')
-// startButton.addEventListener('click', startQuiz)
+let startButton = document.getElementById('start-btn')
+startButton.addEventListener('click', startQuiz)
 
 /**
- *   Function used to start the quiz with a score of nil and picking up the first question
+ *  Function used to start the quiz with a score of nil and picking up the first question
  */
-  function startQuiz () {
-    console.log('Started quiz')
-    score = 0;
-    questionCounter = 0;
-    questionOptions = [...questions];
-    getNextQuestion();
-  }
+function startQuiz () {
+  console.log('Started quiz')
+  score = 0;
+  questionCounter = 0;
+  questionOptions = [...questions];
+  getNextQuestion();
+}
   
-  /**
-   * getNextQuestion function used to keep track of the score returning the quiz page at the end 
-   */
-  function getNextQuestion () {
-      if (questionOptions.length === 0 || questionCounter > max_questions) {
-          localStorage.setItem('mostRecentScore', score)
-          return window.location.assign('quiz.html')
-      }
+// End of quiz variables and modals
+let userScore = document.getElementById('user-score');
+let overlayEnd = document.getElementById('end-overlay');
+let endScreen = document.getElementById('end-screen-modal');
+let restartQuiz = document.getElementById('restart');
 
-      // changes value of width property for progress bar relating to question number
-      quizProgressMax.style.width = `${(questionCounter/max_questions) * 100}%`
-      // changes the question number count out of 10 increasing by +1
-      questionCounter++
-      quizProgressTitle.innerText = `Question ${questionCounter} of ${max_questions}`
+/**
+ * Function that displays the end of quiz modal and score
+ */
+function endQuiz() {
+  overlayEnd.style.display = "block";
+  endScreen.style.display = "block";
+  userScore.innerText = quizScore.innerText;
+}
 
-      // randomises the questions displayed to the user so that they don't repeat order
-      let questionsIndex = Math.floor(Math.random() * questionOptions.length);
-      currentQuestion = questionOptions[questionsIndex]
-      question.innerText = currentQuestion.question
-      
-      // for loop to go through each question answer
-      options.forEach(option => {
-          let number = option.dataset['number']
-          option.innerText = currentQuestion['option' + number]
-      })
-      
-      // change content of array by removing previous question
-      questionOptions.splice(questionsIndex, 1);
-      correctAnswer = true;
-  }
+// Restart quiz on user click
+restartQuiz.addEventListener('click', function () {
+  return location.assign('/quiz.html')
+})
+
+/**
+ * getNextQuestion function used to keep track of the score returning the quiz page at the end 
+ */
+function getNextQuestion () {
+    if (questionOptions.length === 0 || questionCounter > max_questions) {
+        localStorage.setItem('mostRecentScore', score)
+        // calls end of quiz modal
+        endQuiz();
+        return
+    }
+
+    // changes value of width property for progress bar relating to question number
+    quizProgressMax.style.width = `${(questionCounter/max_questions) * 100}%`
+    // changes the question number count out of 10 increasing by +1
+    questionCounter++
+    quizProgressTitle.innerText = `Question ${questionCounter} of ${max_questions}`
+
+    // randomises the questions displayed to the user so that they don't repeat order
+    let questionsIndex = Math.floor(Math.random() * questionOptions.length);
+    currentQuestion = questionOptions[questionsIndex]
+    question.innerText = currentQuestion.question
+    
+    // for loop to go through each question answer
+    options.forEach(option => {
+        let number = option.dataset['number']
+        option.innerText = currentQuestion['option' + number]
+    })
+    
+    // change content of array by removing previous question
+    questionOptions.splice(questionsIndex, 1);
+    correctAnswer = true;
+}
   
-  // See readme for credits with tutorial used to get guidance for the following arrow function
+  /* See readme for credits with code below. Tutorial used to get guidance for the following 
+  arrow function, adapted and modified to my requirements */
+  
+  /* Loops through answer options and adds a user click event about the targeted option
+  which is then referenced against the dataset */
   options.forEach(option => {
     option.addEventListener('click', event => {
         if(!correctAnswer) return
@@ -109,7 +135,8 @@ let correctAnswer = true
     quizScore.innerText = score
   }
 
-// Array for list of questions to appear in quiz
+/* Array for list of questions to appear in quiz, this will get picked up and randomised 
+through a Math object */
 let questions = 
 [
     {
