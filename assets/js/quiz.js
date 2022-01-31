4bbd837f98ab1d3d7b3d23dc98b90c6d4e91018b
Richard J Harding <richard.jharding@hotmail.co.uk>
2022-01-31 16:36:08 +0000

    Refactor: Amend classes, variables and question options

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,37 +1,41 @@
-/* File used by quiz.html in order to display a quiz which is made up of 10
-questions for the user to answer. The quiz will be run with a progress bar and 
-question count showing the progress of the quiz so that the user can guage how 
-many questions are left. The score will increment at the top to give feedback
-to the user on how well they are doing.
-*/
+// /* File used by quiz.html in order to display a quiz which is made up of 10
+// questions for the user to answer. The quiz will be run with a progress bar and 
+// question count showing the progress of the quiz so that the user can guage how 
+// many questions are left. The score will increment at the top to give feedback
+// to the user on how well they are doing.
+// */
 
-// List of variables used for the Quiz
+// // List of variables used for the Quiz
 
 // score variable
-let quizScore = document.getElementById('#score');
+let quizScore = document.getElementById('score');
 let points_score = 10;
 let score = 0;
 
 // progress bar variables - Resource used for help: https://javascript.plainenglish.io/building-a-progress-bar-in-css-js-html-from-scratch-6449da06042
-let quizProgressTitle = document.getElementById('#quiz-prog-title');
-let quizProgressMax = document.getElementById('#quiz-progress-max');
+let quizProgressTitle = document.getElementById('quiz-progress-title');
+let quizProgressMax = document.getElementById('quiz-progress-max');
 let questionCounter = 0;
 let max_questions = 10;
 
 // question variables
-let question = document.getElementbyId('#question');
+let question = document.getElementById('question');
 let currentQuestion = {};
-let questionCounter = 0;
 let questionOptions = [];
 
 // answer variables
-let options = Array.from(document.getElementsByClassName('.answer-option'));
+let options = Array.from(document.querySelectorAll('.answer-option'));
 let correctAnswer = true
+
+// Clicking on the start button will begin quiz
+let startButton = document.getElementById('start-btn')
+startButton.addEventListener('click', startQuiz)
 
 /**
  * startGame function used to start the quiz with a score of nil and picking up the first question
  */
- function startQuiz () {
+  function startQuiz () {
+    console.log('Started quiz')
     score = 0;
     questionCounter = 0;
     questionOptions = [...questions];
@@ -49,19 +53,19 @@ let correctAnswer = true
   
       questionCounter++
       // changes value of width property for progress bar relating to question number
-      quizProgressMax.style.width = `${(questionCounter / max_questions) * 100}%`;
+      quizProgressMax.style.width = `${(questionCounter/max_questions) * 100}%`
       // changes the question number count out of 10
-      quizProgressTitle.innerText = `Question ${questionCounter} of ${max_questions}`;
+      quizProgressTitle.innerText = `Question ${questionCounter} of ${max_questions}`
 
       // randomises the questions displayed to the user so that they don't repeat order
       let questionsIndex = Math.floor(Math.random() * questionOptions.length);
-      currentQuestion = questionOptions[questionIndex]
+      currentQuestion = questionOptions[questionsIndex]
       question.innerText = currentQuestion.question
       
       // for loop to go through each question answer
-      answerOptions.forEach(option => {
-          let value = option.dataset['value']
-          option.innerText = currentQuestion['option' + value]
+      options.forEach(option => {
+          let number = option.dataset['number']
+          option.innerText = currentQuestion['option' + number]
       })
       
       // change content of array for correct answer
@@ -69,67 +73,114 @@ let correctAnswer = true
       correctAnswer = true;
   }
 
+  options.forEach(option => {
+    option.addEventListener('click', e => {
+        if(!correctAnswer) return
+
+        correctAnswer = false
+        let selectOption = e.target
+        let selectAnswer = selectOption.dataset['number']
+        let applyClass = selectAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(applyClass === 'correct') {
+          incrementScore(points_score)
+        }
+
+        selectOption.parentElement.classList.add(applyClass)
+    })
+  })
+
 // Array for questions to appear in quiz
 var questions = 
 [
     {
         // Question 1
         question: "Which of these mountains are NOT in Colombia?",
-        options: ["Pico Simón Bolívar", "Ritacuba Blanco", "Ben Nevis", "Pico Cristóbal Colón"],
+        option1: "Pico Simón Bolívar",
+        option2: "Ritacuba Blanco",
+        option3: "Ben Nevis",
+        option4: "Pico Cristóbal Colón",
         answer: 3
     },
     {
         // Question 2
         question: "Colombia produces 60% of the world's supply of which precious stone?",
-        options: ["Garnet", "Emerald", "Diamond", "Topaz"],
+        option1: "Garnet",
+        option2: "Emerald",
+        option3: "Diamond",
+        option4: "Topaz",
         answer: 2
     },
     {
         // Question 3
         question: "What is the capital city of Colombia?",
-        options: ["Bogotá", "Medellin", "Cali", "Cartagena"],
+        option1: "Bogotá",
+        option2: "Medellin",
+        option3: "Cali",
+        option4: "Cartagena",
         answer: 1
     },
     {
         // Question 4
         question: "Which waterfall can be found in the city of Bogotá?",
-        options: ["Victoria Falls", "Tugela Falls", "Angel Falls", "Tequendama Falls"],
+        option1: "Victoria Falls",
+        option2: "Tugela Falls",
+        option3: "Angel Falls",
+        option4: "Tequendama Falls",
         answer: 4
     },
     {
         // Question 5
         question: "What is Cumbia?",
-        options: ["A type of drink", "A type of music", "A type of bird", "A type of food"],
+        option1: "A type of drink",
+        option2: "A type of music",
+        option3: "A type of bird",
+        option4: "A type of food",
         answer: 2
     },
     {
         // Question 6
         question: "Which of these countries does NOT share a border with Colombia?",
-        options: ["Peru", "Venezuela", "Mexico", "Ecuador"],
+        option1: "Peru",
+        option2: "Venezuela",
+        option3: "Mexico",
+        option4: "Ecuador",
         answer: 3
     },
     {
         // Question 7
         question: "Which of these is the main river of Colombia?",
-        options: ["Orinoco", "Amazonas", "Atrato", "Magdalena"],
+        option1: "Orinoco",
+        option2: "Amazonas", 
+        option3: "Atrato",
+        option4: "Magdalena",
         answer: 4
     },
     {
         // Question 8
         question: "What is the national animal of Colombia?",
-        options: ["Condor", "Jaguar", "Toucan", "Giant Anteater"],
+        option1: "Condor",
+        option2: "Jaguar",
+        option3: "Toucan",
+        option4: "Giant Anteater",
         answer: 1
     },
     {
         // Question 9
         question: "What material is the 'Sombrero vueltiao' made of?",
-        options: ["Cotton", "Plastic", "Straw", "Paper"],
+        option1: "Cotton", 
+        option2: "Plastic",
+        option3: "Straw",
+        option4: "Paper",
         answer: 3
     },
     {
         // Question 10
         question: "What is the official language of Colombia?",
-        options: ["Portuguese", "Spanish", "English", "French"],
+        option1: "Portuguese",
+        option2: "Spanish",
+        option3: "English",
+        option4: "French",
         answer: 2
     },
 ]
