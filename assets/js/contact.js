95dfe6acf982fcee2f2e11c8b3d7a2fed72f2b96
Richard J Harding <richard.jharding@hotmail.co.uk>
2022-02-02 17:49:58 +0000

    Refactor: Clean up code to be more readable

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,31 +1,57 @@
 // Resource used for guidance on form validation: https://www.codebrainer.com/blog/contact-form-in-javascript
 
 // global variables
-let form = document.querySelector('form')
+let form = document.querySelector('form');
 let nameInput = document.getElementById('name');
-let formFeedback = document.querySelector('.form-feedback')
+let emailInput = document.getElementById('email');
+let formFeedback = document.querySelector('.form-feedback');
+
 let isValid = false;
 
+let inputs = [
+  nameInput,
+  emailInput,
+]
 /**
  * Function to produce an error prompt and form field border 
  * color change of red if first and/or last name hasn't been input
  */
 function inputValidate() {
-  nameInput.classList.remove('invalid');
-  nameInput.nextElementSibling.classList.add('hidden');
   isValid = true;
+  resetInput(nameInput);
+  resetInput(emailInput);
 
   if (!nameInput.value) {
-    nameInput.nextElementSibling.classList.remove('hidden')
-    nameInput.classList.add('invalid')
     isValid = false;
+    invalidateInput(nameInput);
+  }
+
+  if (!isEmail(emailInput.value)) {
+    isValid = false;
+    invalidateInput(emailInput);
   }
 }
 
+/**
+ * 
+ * Helper function for inputValidate to clean code
+ */
+function resetInput (element) {
+  element.classList.remove('invalid');
+  element.nextElementSibling.classList.add('hidden');
+}
+
+function invalidateInput (element) {
+  element.nextElementSibling.classList.remove('hidden')
+  element.classList.add('invalid')
+}
 // Event listener to remove red error prompt from border if input
-nameInput.addEventListener('input', () => {
-  inputValidate();
-})
+inputs.forEach(input => {
+  input.addEventListener('input', () => {
+    inputValidate();
+  });
+});
+
 
 
 // Prevents form from submitting by default for validation control
@@ -39,7 +65,14 @@ form.addEventListener('submit', (e) => {
   }
 });
 
-
+/**
+ *  Function to check that the string is an email with regex epression
+ */
+ function isEmail(email) {
+  let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  
+  return regex.test(String(email).toLowerCase());
+}
 
 
 // let fields = {}
@@ -76,14 +109,14 @@ form.addEventListener('submit', (e) => {
 //   return (value.length > 0);
 // }
 
-// /**
-//  *  Function to check that the string is an email with regex epression
-//  */
-// function isEmail(email) {
-//   let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+/**
+ *  Function to check that the string is an email with regex epression
+ */
+function isEmail(email) {
+  let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
   
-//   return regex.test(String(email).toLowerCase());
-// }
+  return regex.test(String(email).toLowerCase());
+}
 
 // /**
 //  * Function to check form condition and feedback any error with red highlight
