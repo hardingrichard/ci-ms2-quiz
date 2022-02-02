8f2e9162850ed701c5377b5850d9d5e6e031e0e0
Richard J Harding <richard.jharding@hotmail.co.uk>
2022-02-02 15:27:12 +0000

    Refactor: Comment out code to test an alternative approach

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,94 +1,110 @@
 // Resource used for guidance on form validation: https://www.codebrainer.com/blog/contact-form-in-javascript
 
-let fields = {}
+// global variables
+let form = document.querySelector('form')
+let firstNameInput = document.getElementById('fname');
+let lastNameInput = document.getElementById('lname');
 
-document.addEventListener("DOMContentLoaded", function () {
-  fields.fName = document.getElementById('fname');
-  fields.lname = document.getElementById('lname');
-  fields.email = document.getElementById('email-field');
-  fields.emailConfirm = document.getElementById('email-field-confirm');
-  fields.message = document.getElementById('message');
-  fields.newsletter = document.getElementById('newsletter');
-})
+// Prevents form from submitting by default for validation control
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log("submit event")
+});
 
-/**
- *  Function to check that field is not empty
- */
-function isNotEmpty(value) {
-  if (value == null || typeof value == 'undefined') return false;
-  return (value.length > 0);
-}
 
-/**
- *  Function to check that the string is an email with regex epression
- */
-function isEmail(email, emailConfirm) {
-  let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-  return regex.test(String(email, emailConfirm).toLowerCase());
-}
 
-/**
- * Function to check form condition and feedback any error with red highlight
- */
-function formValidate(formField, validateFunction) {
-  if (formField == null) return false;
 
-  let isFormFieldValid = validateFunction(formField.value)
-  if (!isFormFieldValid) {
-    formField.className = 'placeholderRed';
-  } else {
-    formField.className = '';
-  }
-  return isFormFieldValid;
-}
+// let fields = {}
 
-/**
- *  Function to check the validity of the contact form combining validations
- */
-function isValid() {
-  let valid = true;
+// document.addEventListener("DOMContentLoaded", function () {
+//   fields.fName = document.getElementById('fname');
+//   fields.lname = document.getElementById('lname');
+//   fields.email = document.getElementById('email-field');
+//   fields.message = document.getElementById('message');
+//   fields.newsletter = document.getElementById('newsletter');
+// })
 
-  valid &= formValidate(fields.fName, isNotEmpty);
-  valid &= formValidate(fields.lName, isNotEmpty);
-  valid &= formValidate(fields.email, isNotEmpty);
-  valid &= formValidate(fields.emailConfirm, isNotEmpty);
-  valid &= formValidate(fields.message, isNotEmpty);
-  valid &= isEmailEqual();
-  return valid;
-}
+// let form = document.getElementById('form');
 
-/**
- * Function to check that the email addresses match and to highlight red if not
- */
-function isEmailEqual() {
-  if (formField.email.value == formField.emailConfirm.value) {
-    formField.email.className = 'placeholderRed';
-    formField.emailConfirm.className = 'placeholderRed';
-    return true;
-  }
-  return false
-}
+// form.addEventListener('submit',(e) => {
+//   let messages = []
+//   if(fields.fname.value === '' || fields.fname.value == null) {
+//     messages.push('First name is required')
+//   }
+//   if(fields.lname.value === '' || fields.lname.value == null) {
+//     messages.push('Last name is required')
+//   }
+//   if(messages.length > 0) {
+//     e.preventDefault()
+//   }
+// })
 
-// User class constructor to combine values into same category
-class User {
-  constructor(fName, lName, email, emailConfirm, newsletter, message) {
-    this.fName = fName;
-    this.lName = lName;
-    this.email = email;
-    this.emailConfirm = emailConfirm;
-    this.newsletter = newsletter;
-    this.message = message;
-  }
-}
+// /**
+//  *  Function to check that form field is not empty
+//  */
+// function isNotEmpty(value) {
+//   if (value == null || typeof value == 'undefined') return false;
 
-/**
- * Function to submit the contact form with validation. Error message to display if invalid.
- */
-function submitForm() {
-  if (isValid()) {
-    let formUser = new User(fName.value, lName.value, email.value, emailConfirm.value, newsletter.checked);
-    alert(`${formUser.fName} thanks for submitting your form!`)
-  } else {
-    alert("There was an error submitting your form. Please make sure all fields are entered correctly and emails match.")
-  }
-}
+//   return (value.length > 0);
+// }
+
+// /**
+//  *  Function to check that the string is an email with regex epression
+//  */
+// function isEmail(email) {
+//   let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  
+//   return regex.test(String(email).toLowerCase());
+// }
+
+// /**
+//  * Function to check form condition and feedback any error with red highlight
+//  */
+// function formValidate(formField, validateFunction) {
+//   if (formField == null) return false;
+
+//   let isFormFieldValid = validateFunction(formField.value)
+//   if (!isFormFieldValid) {
+//     formField.className = 'placeholderRed';
+//   } else {
+//     formField.className = '';
+//   }
+//   return isFormFieldValid;
+// }
+
+// /**
+//  *  Function to check the validity of the contact form combining validations
+//  */
+// function isValid() {
+//   let valid = true;
+
+//   valid &= formValidate(fields.fName, isNotEmpty);
+//   valid &= formValidate(fields.lName, isNotEmpty);
+//   valid &= formValidate(fields.email, isNotEmpty);
+//   valid &= formValidate(fields.message, isNotEmpty);
+//   return valid;
+// }
+
+// // User class constructor to combine values into same category
+// class User {
+//   constructor(fName, lName, email, newsletter, message) {
+//     this.fName = fName;
+//     this.lName = lName;
+//     this.email = email;
+//     this.newsletter = newsletter;
+//     this.message = message;
+//   }
+// }
+
+
+// /**
+//  * Function to submit the contact form with validation. Error message to display if invalid.
+//  */
+// function submitForm() {
+//   if (isValid()) {
+//     let formUser = new User(fName.value, lName.value, email.value, newsletter.checked);
+//     alert(`${formUser.fName} thanks for submitting your form!`)
+//   } else {
+//     alert("There was an error submitting your form. Please make sure all fields are entered.")
+//   }
+// }
