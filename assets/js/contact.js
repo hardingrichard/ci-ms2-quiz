c085d480a5489bb228b04031f5e2b8c2aceffc9c
Richard J Harding <richard.jharding@hotmail.co.uk>
2022-02-03 01:25:07 +0000

    Refactor: Clean up code

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,11 +1,12 @@
-// Resource used for guidance on form validation: https://www.codebrainer.com/blog/contact-form-in-javascript
+/* Resource used for guidance on form validation:
+https://www.codebrainer.com/blog/contact-form-in-javascript */
 
 // global variables
-let form = document.querySelector('form');
-let nameInput = document.getElementById('name');
-let emailInput = document.getElementById('email');
-let messageInput = document.getElementById('message')
-let formFeedback = document.querySelector('.form-feedback');
+let form = document.querySelector("form");
+let nameInput = document.getElementById("name");
+let emailInput = document.getElementById("email");
+let messageInput = document.getElementById("message");
+let formFeedback = document.querySelector(".form-feedback");
 
 let isValid = false;
 let isValidateActive = false;
@@ -14,11 +15,11 @@ let isValidateActive = false;
 let inputs = [
   nameInput,
   emailInput,
-  messageInput,
-]
+  messageInput
+];
 
 /**
- * Function to produce an error prompt and form field border 
+ * Function to produce an error prompt and form field border
  * color change of red if first and/or last name hasn't been input
  */
 function inputValidate() {
@@ -37,7 +38,7 @@ function inputValidate() {
     isValid = false;
     invalidateInput(emailInput);
   }
-  
+
   if (!messageInput.value) {
     isValid = false;
     invalidateInput(messageInput);
@@ -47,53 +48,54 @@ function inputValidate() {
 /**
  * Helper function for inputValidate to clean up code for easier reading
  */
-function resetInput (element) {
-  element.classList.remove('invalid');
-  element.nextElementSibling.classList.add('hidden');
+function resetInput(element) {
+  element.classList.remove("invalid");
+  element.nextElementSibling.classList.add("hidden");
 }
 
-function invalidateInput (element) {
-  element.nextElementSibling.classList.remove('hidden')
-  element.classList.add('invalid')
+function invalidateInput(element) {
+  element.nextElementSibling.classList.remove("hidden");
+  element.classList.add("invalid");
 }
 
-// Event listener for each loop through array and remove red error prompt from border if input
+/* Event listener for each loop through array and remove red error prompt 
+from border if input */
 inputs.forEach(input => {
-  input.addEventListener('input', () => {
+  input.addEventListener("input", () => {
     inputValidate();
   });
 });
 
-// Event listener to prevent form from submitting by default for validation control
-form.addEventListener('submit', (e) => {
+// Event listener to prevent form submitting by default for validation control
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   isValidateActive = true;
   inputValidate();
-  
+
   if (isValid) {
     form.remove();
-    formFeedback.classList.remove('hidden');
+    formFeedback.classList.remove("hidden");
   }
 });
 
 /**
  *  Function to check that the string is an email with regex epression
  */
- function isEmail(email) {
+function isEmail(email) {
   let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-  
+
   return regex.test(String(email).toLowerCase());
 }
 
 // emailJS code taken and modified from emailJS.com tutorial
-window.onload = function() {
-  document.getElementById('contact-form').addEventListener('submit', function(event) {
-      emailjs.init("user_ylIxWxgFUymrhtkjDo5o3");
-      emailjs.sendForm('contact_service', 'contact_form', this)
-          .then(function() {
-              console.log('SUCCESS!');
-          }, function(error) {
-              console.log('FAILED...', error);
-          });
+window.onload = function () {
+  document.getElementById("contact-form").addEventListener("submit", function (event) {
+    emailjs.init("user_ylIxWxgFUymrhtkjDo5o3");
+    emailjs.sendForm("contact_service", "contact_form", this)
+      .then(function () {
+        console.log("SUCCESS!");
+      }, function (error) {
+        console.log("FAILED...", error);
+      });
   });
-}
+};
